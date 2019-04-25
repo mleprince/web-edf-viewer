f7430080a9e4324ff92c5ef1cddef069edafa99b
Maxime Leprince <maxime.leprince@bioserenity.com>
2019-04-25 11:21:08 +0800

    update travis.yml

--- edf-viewer-worker/build.sh
+++ edf-viewer-worker/build.sh
@@ -1,3 +1,4 @@
+cargo build --target wasm32-unknown-unknown --release
 wasm-pack build --release --target=web
 rm -rf ../edf-viewer-app/public/assembly
 cp -a  ./pkg/. ../edf-viewer-app/public/assembly/
